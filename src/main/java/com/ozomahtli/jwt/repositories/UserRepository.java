bf5f845c89f689210b9ad4c09bded2bdf51ee794
Your Name <diegod9405@gmail.com>
2024-09-24 15:58:56 -0600

    first commit

---
+++ src/main/java/com/ozomahtli/jwt/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ozomahtli.jwt.repositories;
+
+import com.ozomahtli.jwt.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/test/java/com/ozomahtli/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozomahtli.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
